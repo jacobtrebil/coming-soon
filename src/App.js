53e4df3d889d1029a018f2ecac8ce67c8104842e
Jacob Trebil <jacobtrebil@yahoo.com>
2024-02-27 21:00:47 -0600

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import { motion } from 'framer-motion';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <motion.div
+        initial="hidden" 
+        whileInView="visible" 
+        transition={{ duration: 0.5 }}       
+        variants={{
+          visible: { opacity: 1, scale: 1 },
+          hidden: { opacity: 0, scale: 0 }
+      }}>
+        <motion.p whileHover={{ scale: 1.05 }} transition={{ type: 'spring', stiffness: 300 }} className="logo">Balla</motion.p>
+        <motion.h1 whileHover={{ scale: 1.05 }} transition={{ type: 'spring', stiffness: 300 }} className="comingSoon">Coming Soon</motion.h1>
+      </motion.div>
   );
 }
 
